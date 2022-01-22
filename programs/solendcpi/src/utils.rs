9d77ff04d516b7e2cc66ecca4ffa47371c93cb0c
RohanXKapur <rohankapurdev@outlook.com>
2022-01-22 20:11:20 +0800

    add constants for global and sol markets on devnet

---
+++ programs/solendcpi/src/utils.rs
@@ -0,0 +1,8 @@
+// Solend global devnet addresses
+pub const DEVNET_SOLEND_PROGRAM: &str = "ALend7Ketfx5bxh6ghsCDXAoDrhvEmsXT3cynB6aPLgx";
+pub const DEVNET_SOLEND_LENDING_MARKET: &str = "GvjoVKNjBvQcFaSKUW1gTE7DxhSpjHbE69umVR5nPuQp";
+
+// Solend devnet SOL market addresses
+pub const DEVNET_SOLEND_SOL_RESERVE: &str = "5VVLD7BQp8y3bTgyF5ezm1ResyMTR3PhYsT4iHFU8Sxz";
+pub const DEVNET_SOLEND_CSOL_COLLATERAL_MINT: &str = "FzwZWRMc3GCqjSrcpVX3ueJc6UpcV6iWWb7ZMsTXE3Gf";
+pub const DEVNET_SOLEND_CSOL_LIQUIDITY_SUPPLY: &str = "furd3XUtjXZ2gRvSsoUts9A5m8cMJNqdsyR2Rt8vY9s";
